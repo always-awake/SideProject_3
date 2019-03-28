a48f1d50e9a2d5f1860bcf0624a5b6761dd2e7d9
leemirim <leemirim9654@gmail.com>
2019-03-28 17:07:35 +0900

    Post모델 생성 및 Admin 등록

--- sideproject/myapp/models.py
+++ sideproject/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.CharField(max_length=20)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+    
